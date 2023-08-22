f09bf528ecb0c555ec629d6c6c076b9e81b3d75a
LazarGerasimov <lazgerasimov@gmail.com>
2023-08-22 17:01:26 +0300

    update getRandomPrompt functionality: - add extra functionality to make sure we do not get the same prompt two times in a row. - although the chance for the above is minimal it would still lead to bad user xp.

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -3,4 +3,9 @@ import { surpriseMePrompts } from '../constants';
 export function getRandomPrompt(prompt) {
     const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
     const randomPrompt = surpriseMePrompts[randomIndex];
+
+    //add extra functionality to make sure we do not get the same prompt twice. 
+    if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+    return randomPrompt;
 }
