0e5fe4b3f15811306c22f55ac947464accda455a
LazarGerasimov <lazgerasimov@gmail.com>
2023-08-22 17:20:37 +0300

    update CreatePost component: - add 3 function scaffolds: -- submit handler -- onChange handler -- handle surpriseMe function - add 2x FormFields with props

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,6 +16,18 @@ function CreatePost() {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+    
+  }
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -26,6 +38,29 @@ function CreatePost() {
             with DALL-E AI and share them with the community</p>
         </div>
       </div>
+
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
     </section>
   )
 }
