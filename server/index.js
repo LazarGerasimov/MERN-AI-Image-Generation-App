f36fbd574442bc2481b40425a86ef12e52683124
LazarGerasimov <lazgerasimov@gmail.com>
2023-08-27 11:01:51 +0300

    add mongodb connection prereq

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect';
+
 dotenv.config(); // allows us to pull envs from dotenv file; 
 
 const app = express(); // Creates an Express application
@@ -13,7 +15,13 @@ app.get('/', async (req, res) => {
 });
 
 const startServer = async () => {
-    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
+
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 startServer();
