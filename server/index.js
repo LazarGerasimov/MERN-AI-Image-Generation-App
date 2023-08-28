a0863cd11a9c325874ced412f2e305ea51fa0fc1
LazarGerasimov <lazgerasimov@gmail.com>
2023-08-28 22:52:20 +0300

    add postRoutes and dalleRoutes in the backend middleware

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config(); // allows us to pull envs from dotenv file; 
 
 const app = express(); // Creates an Express application
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));   
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hello from Dall-E');
